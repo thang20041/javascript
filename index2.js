6df56f12804f0422058f31425ec6c629d32bae6e
thang20041 <giangthang200420011@gmail.com>
2023-08-07 13:43:27 +0700

    fifle Javascript

---
+++ index2.js
@@ -0,0 +1,8 @@
+let a=5, b = 10;
+function bam(){
+let c = document.getElementById ('H').innerHTML = (a + b);
+    document.write (c);
+    alert (c);
+    console.log (c)
+    
+}
---
+++ index4.js
@@ -0,0 +1,10 @@
+let A = 100, B = 20;
+function SUM() {
+let A = + document.getElementById('a').value
+let B = + document.getElementById ('b').value
+let C = document.getElementById ('e').innerHTML = (A + B);
+console.log (C);
+alert (C);
+
+
+}
