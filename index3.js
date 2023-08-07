f3d5379f57a0278cfe54fed2785b55619e5ca2f7
thang20041 <giangthang200420011@gmail.com>
2023-08-07 20:24:56 +0700

    file ok

---
+++ index3.js
@@ -0,0 +1,8 @@
+
+function tinhtoan(){
+    let a  = +document.getElementById ('A').value
+    let b  = +document.getElementById ('B').value
+    let e  = a +b;
+    alert ("A = "+ a +"; B = " + b + ";Tổng là:" + e);
+
+}
